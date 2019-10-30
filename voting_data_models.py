456ddd533aeaf6a97137a5b9c3d94f6db1c824bd
jmelton22 <jmelton22@gmail.com>
2019-10-30 18:06:08 -0400

    Added loop for each dataset version for Decision Tree and Naive Bayes

--- voting_data_models.py
+++ voting_data_models.py
@@ -17,18 +17,18 @@ def print_metrics(labels, preds):
     scores = metrics.precision_recall_fscore_support(labels, preds)
     conf = metrics.confusion_matrix(labels, preds)
     print(' ' * 4 + 'Confusion Matrix')
-    print(' ' * 17 + 'Predict Republican    Predict Democrat')
-    print('Actual Republican         {}                 {}'.format(conf[0, 0], conf[0, 1]))
-    print('Actual Democrat            {}                 {}'.format(conf[1, 0], conf[1, 1]))
+    print(' ' * 19 + 'Predict Republican    Predict Democrat')
+    print('Actual Republican         {}                   {}'.format(conf[0, 0], conf[0, 1]))
+    print('Actual Democrat            {}                  {}'.format(conf[1, 0], conf[1, 1]))
     print()
     print('Accuracy: {0:.3f}'.format(metrics.accuracy_score(labels, preds)))
     print()
     print(' ' * 4 + 'Classification Report')
     print(' ' * 11 + 'Republican    Democrat')
-    print('Num cases    {}           {}'.format(scores[3][0], scores[3][1]))
-    print('Precision    {0:.2f}       {0:.2f}'.format(scores[0][0], scores[0][1]))
-    print('Recall       {0:.2f}       {0:.2f}'.format(scores[1][0], scores[1][1]))
-    print('F1 Score     {0:.2f}       {0:.2f}'.format(scores[2][0], scores[2][1]))
+    print('Num cases    {}             {}'.format(scores[3][0], scores[3][1]))
+    print('Precision    {0:.2f}         {0:.2f}'.format(scores[0][0], scores[0][1]))
+    print('Recall       {0:.2f}         {0:.2f}'.format(scores[1][0], scores[1][1]))
+    print('F1 Score     {0:.2f}         {0:.2f}'.format(scores[2][0], scores[2][1]))
 
 
 def print_cv_scores(f1, precision, recall):
@@ -59,18 +59,10 @@ voting_data_v1 = voting_data.dropna(axis=0, how='any')
 labels_v1 = voting_data_v1['Class Name']
 features_v1 = voting_data_v1.drop('Class Name', axis=1)
 
-X1_train, X1_test, y1_train, y1_test = ms.train_test_split(features_v1, labels_v1,
-                                                           test_size=0.2,
-                                                           random_state=123)
-
 # Version 2: Replace NaN with third category
 voting_data_v2 = voting_data.fillna(2)
 labels_v2 = voting_data_v2['Class Name']
 features_v2 = voting_data_v2.drop('Class Name', axis=1)
-
-X2_train, X2_test, y2_train, y2_test = ms.train_test_split(features_v2, labels_v2,
-                                                           test_size=0.2,
-                                                           random_state=123)
 
 # Version 3: Replace NaN with mode (most frequent)
 labels_v3 = voting_data['Class Name']
@@ -79,38 +71,40 @@ features_v3 = voting_data.drop('Class Name', axis=1)
 imp = SimpleImputer(strategy='most_frequent')
 features_v3 = imp.fit_transform(features_v3)
 
-X3_train, X3_test, y3_train, y3_test = ms.train_test_split(features_v3, labels_v3,
+for i, (features, labels) in enumerate([(features_v1, labels_v1), (features_v2, labels_v2), (features_v3, labels_v3)]):
+    X_train, X_test, y_train, y_test = ms.train_test_split(features, labels,
                                                            test_size=0.2,
                                                            random_state=123)
 
-# Version 1: Decision Tree
-print('Version 1: Decision Tree')
-tree_v1 = DecisionTreeClassifier()
-tree_v1.fit(X1_train, y1_train)
+    # Decision Tree model
+    print('Version {}: Decision Tree'.format(i+1))
+    tree = DecisionTreeClassifier()
+    tree.fit(X_train, y_train)
 
-y1_pred = tree_v1.predict(X1_test)
-print_metrics(y1_test, y1_pred)
-print()
+    y_pred_tree = tree.predict(X_test)
+    print_metrics(y_test, y_pred_tree)
+    print()
 
-tree1_cv_f1 = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='f1')
-tree1_cv_precision = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='precision')
-tree1_cv_recall = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='recall')
+    tree_cv_f1 = ms.cross_val_score(tree, features, labels, cv=5, scoring='f1')
+    tree_cv_precision = ms.cross_val_score(tree, features, labels, cv=5, scoring='precision')
+    tree_cv_recall = ms.cross_val_score(tree, features, labels, cv=5, scoring='recall')
 
-print_cv_scores(tree1_cv_f1, tree1_cv_precision, tree1_cv_recall)
-print('-' * 25)
+    print_cv_scores(tree_cv_f1, tree_cv_precision, tree_cv_recall)
+    print('-' * 25)
 
-# Version 1: Naive Bayes
-print('Version 1: Naive Bayes')
-bnb_v1 = BernoulliNB()
-bnb_v1.fit(X1_train, y1_train)
+    # Naive Bayes model
+    print('Version {}: Naive Bayes'.format(i+1))
+    bnb = BernoulliNB()
+    bnb.fit(X_train, y_train)
 
-y2_pred = bnb_v1.predict(X1_test)
-print_metrics(y1_test, y1_pred)
-print()
+    y_pred_nb = bnb.predict(X_test)
+    print_metrics(y_test, y_pred_nb)
+    print()
 
-nb1_cv_f1 = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='f1')
-nb1_cv_precision = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='precision')
-nb1_cv_recall = ms.cross_val_score(tree_v1, features_v1, labels_v1, cv=5, scoring='recall')
+    nb_cv_f1 = ms.cross_val_score(tree, features, labels, cv=5, scoring='f1')
+    nb_cv_precision = ms.cross_val_score(tree, features, labels, cv=5, scoring='precision')
+    nb_cv_recall = ms.cross_val_score(tree, features, labels, cv=5, scoring='recall')
 
-print_cv_scores(nb1_cv_f1, nb1_cv_precision, nb1_cv_recall)
-print('-' * 50)
+    print_cv_scores(nb_cv_f1, nb_cv_precision, nb_cv_recall)
+    print('-' * 50)
+    print('-' * 50)
