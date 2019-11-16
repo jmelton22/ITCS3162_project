9defd3fd1b348ad52b8c5afacad0f3ef13341682
jmelton22 <jmelton22@gmail.com>
2019-11-16 13:16:40 -0500

    More comments, reorganizing

--- Part_2/page_rank.py
+++ Part_2/page_rank.py
@@ -20,12 +20,12 @@ with open('as-caida20071105.txt') as f:
 
         # For graph version 2:
         # If relationship is 1, retain edge direction
+        # If relationship is -1, reverse edge direction
+        # Else, randomize edge direction
         if relation == 1:
             graph_v2.add_edge(source, target)
-        # If relationship is -1, reverse edge direction
         elif relation == -1:
             graph_v2.add_edge(target, source)
-        # Else, randomize edge direction
         else:
             graph_v2.add_edge(source, target) if choice([True, False]) else graph_v2.add_edge(target, source)
 
@@ -35,6 +35,8 @@ for i, graph in enumerate([graph_v1, graph_v2]):
     print('Number of edges:', graph.number_of_edges())
     print('Number of nodes:', graph.number_of_nodes())
     pr = nx.pagerank(graph, alpha=0.9)
+
+    # Sort nodes based on page rank value
     sorted_pr = sorted(pr.items(), key=lambda x: x[1], reverse=True)
 
     print('Top 10 ranked nodes:')
